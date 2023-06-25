f9e1d863fe0ac4335f1ccb3c4a98c256b27ad460
nishiki-7 <hika.create@gmail.com>
2023-06-25 19:30:21 +0900

    0625作業分

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Post from "./components/Post/Post.js"
 import Input from "./components/Input/Input.js"
 
 function App() {
+  // Appコンポーネントで、チャットの内容を格納する配列を定義する
+  const [chatTimeline, setChatTimeline] = useState([]); 
 
   const [inputText, setInputText] = useState("");
   const onChangeInputText = (event) => setInputText(event.target.value);
 
   // 送信ボタンをクリックしたら、テキストを追加する処理
   const onClickSend = () => {
+    // chatTimeline配列を渡して、配列の最後に値を格納する→Todoアプリを参考に
     const sendText = inputText;
     setInputText(sendText);
     console.log(inputText + "が送信されました！");
